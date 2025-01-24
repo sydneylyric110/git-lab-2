c90a4ce179322dd8785010020cde95c7acf958ef
Sydney <sk292023@ohio.edu>
2025-01-24 16:06:29 -0500

    initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*      
+        file: git-lab-program.cc
+        author: SYdney Knisley
+        date: 01/24/25
+        description: working on github in lab 
+*/ 
+
+#include <iostream> 
+#include <iomanip> 
+#include <cstdlib> 
+using namespace std;
+
+int main(int argc, char const *argv[]) { 
+
+    cout << "Hello! Git!" << endl;
+    return 0; 
+}
